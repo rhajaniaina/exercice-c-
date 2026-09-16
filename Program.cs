15f8e833263e723c341af644967f74e2dd5447bc
Therealhadz <hajaniainakely@gmail.com>
2026-09-16 21:04:58 +0200

    Exercice 3 terminé

--- Program.cs
+++ Program.cs
@@ -1,22 +1,28 @@
-﻿decimal Crediter(decimal solde, decimal montant)
+﻿Compte CompteFadel = new Compte(7897, "Fadel", 2000);
+Compte CompteAlvin = new Compte(9789, "Alvin", 4000);   
+
+Console.WriteLine("=== Compte Fadel ===");
+Console.WriteLine("Numéro : " + CompteFadel.numero);
+Console.WriteLine("Titulaire : " + CompteFadel.titulaire);
+Console.WriteLine("Solde : " + CompteFadel.solde);
+
+Console.WriteLine();
+
+Console.WriteLine("=== Compte Alvin ===");
+Console.WriteLine("Numéro : " + CompteAlvin.numero);
+Console.WriteLine("Titulaire : " + CompteAlvin.titulaire);
+Console.WriteLine("Solde : " + CompteAlvin.solde);
+
+public class Compte
 {
-    if (montant > 0)
-    {
-        solde += montant;
-    }
-    else
-    {
-        Console.WriteLine("Montant invalide");
-    }
+    public decimal numero { get; }
+    public string titulaire { get; }
+    public decimal solde { get; private set; }
 
-    return solde;
+    public Compte(decimal Numero, string Titulaire, decimal Solde)
+    {
+        numero = Numero;
+        titulaire = Titulaire;
+        solde = Solde;
+    }
 }
-
-
-// Test de la méthode
-decimal solde = 1000m;
-decimal montant = 150m;
-
-solde = Crediter(solde, montant);
-
-Console.WriteLine($"Solde : {solde}");
