50b9140467d83f96424f4fcf0b8cd8b57e32ed4e
Therealhadz <hajaniainakely@gmail.com>
2026-09-18 12:01:38 +0200

    Exercice 9 terminé

--- Program.cs
+++ Program.cs
@@ -1,15 +1,6 @@
 ﻿Compte CompteFadel = new Compte("7897", "Fadel", 2000);
-Compte CompteAlvin = new Compte("9789", "Alvin", 4000);   
+Compte CompteAlvin = CompteFadel;  
 
-Console.WriteLine("=== Compte Fadel ===");
-Console.WriteLine("Numéro : " + CompteFadel.numero);
-Console.WriteLine("Titulaire : " + CompteFadel.titulaire);
-Console.WriteLine("Solde : " + CompteFadel.solde);
+CompteAlvin.Crediter(500m);
 
-Console.WriteLine();
-
-Console.WriteLine("=== Compte Alvin ===");
-Console.WriteLine("Numéro : " + CompteAlvin.numero);
-Console.WriteLine("Titulaire : " + CompteAlvin.titulaire);
-Console.WriteLine("Solde : " + CompteAlvin.solde);
-
+Console.WriteLine(CompteFadel.solde);
---
+++ produit.cs
@@ -0,0 +1,52 @@
+public class Produit
+{
+    public string reference { get; }
+    public string nom { get; private set;}
+    public decimal prix { get; private set;}
+    public decimal quantite {get; private set;}
+
+    public Produit(string Reference, string Nom, decimal Prix,decimal Quantite)
+    {
+        if (Reference == null)
+        {
+            throw new ArgumentException("un produit ne peut etre créer sans référence");
+        }
+
+        if (Prix < 0)
+        {
+            throw new ArgumentException("le Prix ne peut etre negatif");
+        }
+        reference = Reference;
+        nom = Nom;
+        prix = Prix;
+        quantite = Quantite;
+    }
+
+    public Produit(string Reference, string Nom, decimal Prix)
+    : this(Reference, Nom, Prix, 0m)
+{
+}
+    
+
+    
+    public void AjouterStock(decimal quantiteAjoutee)
+    {
+        if (quantiteAjoutee < 0)
+        {
+            throw new ArgumentException("le stock ajouté ne peut etre negatif");
+        }
+        quantite = quantite + quantiteAjoutee;
+    }
+
+    public void RetirerStock(decimal quantiteRetirer)
+    {
+        if (quantiteRetirer > quantite)
+    {
+        throw new ArgumentException("Stock insuffisant");
+    }
+        quantite = quantite - quantiteRetirer;
+    }
+
+
+
+}
